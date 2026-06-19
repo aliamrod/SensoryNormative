55bfab2f5bc2d518cbe6be4f602fcd0ce52b6ac5
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2026-06-19 15:37:57 +0200

    Update 1_vol2surf.sh

--- 1-preprocessing/surface-projection/1_vol2surf.sh
+++ 1-preprocessing/surface-projection/1_vol2surf.sh
@@ -1,1 +1,13 @@
+# Author: Alia Mahama-Rodriguez
+# Late Update: 19 Jun 2027
 
+# This step converts fMRIPrep volumetric outputs in MNI152 volumetric space into left- and right-hemisphere surface files in fsaverage space. 
+# The workflow first resamples the MNI152 image onto FreeSurfer fsaverage anatomical volume space using `mri_vol2vol`. It then projects the fsaverage 
+# cortical surface using `mri_surf2surf`.
+
+# Input
+# - MNI152-space NIfTI image (.nii.gz)
+
+
+# Output:
+# - Left and right hemisphere surface files in fsaverage space (lh.fsaverage.mgh + rh.fsaverage.mgh). 
